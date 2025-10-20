d982252adff65598602ef6ec1e80995b9a4d0066
serkam31 <223928104+serkam31@users.noreply.github.com>
2025-10-20 14:41:45 +0000

    feat: C code that print a sentence

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * main - prints a sentence using puts
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
