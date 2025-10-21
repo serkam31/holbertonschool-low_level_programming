ee9cd8ba2c3f093b68267d5169d183df115e35a9
serkam31 <223928104+serkam31@users.noreply.github.com>
2025-10-21 22:22:14 +0000

    feat:Prints all single digit numbers of base 16

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 /**
- * main - Prints all single digit numbers of base 10
+ * main - Prints all single digit numbers of base 16
  * Return: Always 0 (Success)
  */
 int main(void)
