d742287fde2d8c66f6fe1399c30aa5e6a9082bf7
serkam31 <223928104+serkam31@users.noreply.github.com>
2025-10-21 22:03:54 +0000

    feat: Prints the lowercase alphabet in reverse

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - Prints the lowercase alphabet in reverse
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet = 'z';
+
+	while (alphabet >= 'a')
+	{
+		putchar(alphabet);
+		alphabet--;
+	}
+	putchar('\n');
+
+	return (0);
+}
