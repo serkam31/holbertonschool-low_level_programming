cbac8d098ca27ff6f473a902a6c3a506c26f8bac
serkam31 <223928104+serkam31@users.noreply.github.com>
2025-10-21 21:33:37 +0000

    feat: print numbers with a script using int

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - Prints all single digit numbers of base 10
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int numbers = 0;
+
+	while (numbers <= 9)
+	{
+		printf("%d", numbers);
+		numbers++;
+	}
+	putchar('\n');
+
+	return (0);
+}
