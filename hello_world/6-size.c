a5ea9d8efcc91ce9630efce1d5e5690c2d8a4e1e
serkam31 <223928104+serkam31@users.noreply.github.com>
2025-10-21 08:31:26 +0000

    fix: delete a letter for the checker

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 
-	printf("Size of a char: %zu bytes\n", sizeof(char));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of an int: %zu bytes\n", sizeof(int));
 	printf("Size of a long int: %zu bytes\n", sizeof(long int));
 	printf("Size of a long long int: %zu bytes\n", sizeof(long long int));
