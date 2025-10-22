08ff855b8a2047f71995bdfb684f0600716b104c
serkam31 <223928104+serkam31@users.noreply.github.com>
2025-10-22 13:33:30 +0000

    feat:  function that prints the alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,15 +1,15 @@
 #include "main.h"
-
 /**
- 
-print_alphabet - prints the alphabet in lowercase, followed by a new line
-Return: void*/
+ * print_alphabet - print alphabet
+ * Return: Always 0 (Success)
+ */
 void print_alphabet(void)
 {
-    char c;
+	char c;
 
-    for (c = 'a'; c <= 'z'; c++)
-        _putchar(c);
+	for (c = 'a'; c <= 'z'; c++)
 
-    _putchar('\n');
+	_putchar(c);
+
+	_putchar('\n');
 }
