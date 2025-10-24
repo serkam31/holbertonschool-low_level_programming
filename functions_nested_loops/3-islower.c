ed258126c5bb3a6f8dfca9498228f36690f1592b
serkam31 <223928104+serkam31@users.noreply.github.com>
2025-10-24 07:31:10 +0000

    feat: checks for lowercase character

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower - checks for lowercase character
+ * @c
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
