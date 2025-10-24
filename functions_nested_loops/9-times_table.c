f144440d5f57564bedaa131d5d0c1a8a3ed6d302
serkam31 <223928104+serkam31@users.noreply.github.com>
2025-10-24 11:50:29 +0000

    feat:prints the 9 times table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table
+ */
+void times_table(void)
+{
+	int n, x, multiple;
+
+	for (n = 0; x <= 9; n++)
+	{
+		for (x = 0; x <= 9; x++)
+		{
+			multiple = n * x;
+			if (x != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (multiple < 10)
+					_putchar(' ');
+			}
+
+			if (multiple >= 10)
+				_putchar((multiple / 10) + '0');
+			_putchar((multiple % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
