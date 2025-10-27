ec4d85af4522687d7aa9d73480bc79009e7f4641
serkam31 <223928104+serkam31@users.noreply.github.com>
2025-10-27 10:31:47 +0000

    feat: print numbers except 2 and 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints the numbers from 0 to 9 except 2 and 4
+ * 
+ * Return: void 
+ */
+void print_most_numbers(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		if (n != 2 && n != 4)
+			_putchar(n + '0');
+		n++;
+	}
+	_putchar('\n');
+}
