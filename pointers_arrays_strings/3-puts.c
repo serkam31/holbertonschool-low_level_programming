432323ca31215dd6d32c54b09b519197e258d7f8
serkam31 <223928104+serkam31@users.noreply.github.com>
2025-10-28 10:59:10 +0000

    feat: function that prints a string, followed by a new line

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - affiche une chaîne de caractères suivie d'un retour à la ligne
+ * @str: chaîne à afficher
+ */
+void _puts(char *str)
+{
+    int i = 0;
+
+    while (str[i] != '\0')
+    {
+        _putchar(str[i]);
+        i++;
+    }
+
+    _putchar('\n');
+}
