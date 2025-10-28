b314351755308c313e3ba22797b98d7005698497
serkam31 <223928104+serkam31@users.noreply.github.com>
2025-10-28 19:29:07 +0000

    feat:  function that prints second half of a string

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include"main.h"
+/**
+ * puts_half - prints second half of a string
+ * @str: pointer to the string to print
+ * Return: void 
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+	int start;
+	int i;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	if (len % 2 == 0)
+	{
+		start = len / 2;
+	}
+
+	else
+	{
+		start = (len + 1) / 2;
+	}
+
+	for (i = start; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
