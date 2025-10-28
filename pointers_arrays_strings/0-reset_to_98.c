cde88e24f044f6edb0f62c458988bfdaaca156f1
serkam31 <223928104+serkam31@users.noreply.github.com>
2025-10-28 08:46:21 +0000

    feat: function that takes a pointer to an int and updates the value

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - met à jour la valeur de n à 98
+ * @n: pointe vers l'entier modifier
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n=98;
+}
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+chuuuuuuuuut
