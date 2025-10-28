8ac5c5683c89030e367a9706e45c3a0caaa31135
serkam31 <223928104+serkam31@users.noreply.github.com>
2025-10-28 13:12:25 +0000

    feat:function that prints a string, in reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - affiche une chaîne de caractères à l'envers
+ * @s: pointe vers la chaîne à afficher
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	int r;
+
+	while (s[i] != '\0')
+		i++;
+
+	r = i - 1;
+	while (r >= 0)
+	{
+		_putchar(s[r]);
+		r--;
+	}
+	
+	_putchar('\n');
+}
