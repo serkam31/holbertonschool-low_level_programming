697e4658a19b4a134d9eff46925f3a28faa85b0a
serkam31 <223928104+serkam31@users.noreply.github.com>
2025-10-28 11:00:18 +0000

    function that prints a string, followed by a new line

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -6,13 +6,13 @@
  */
 void _puts(char *str)
 {
-    int i = 0;
+	int i = 0;
 
-    while (str[i] != '\0')
-    {
-        _putchar(str[i]);
-        i++;
-    }
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
 
-    _putchar('\n');
+	_putchar('\n');
 }
