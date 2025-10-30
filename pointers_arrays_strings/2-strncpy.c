5d96887ceee2ae4b409b0d6faaffe1473ece9012
serkam31 <223928104+serkam31@users.noreply.github.com>
2025-10-30 15:27:21 +0000

    feat: function that copies a string

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: pointer to the destination buffer
+ * @src: pointer to the source string
+ * @n: maximum number of bytes to copy
+ *
+ * Return: pointer to dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+
+	return (dest);
+}
