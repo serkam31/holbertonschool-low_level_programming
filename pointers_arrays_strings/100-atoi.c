05fe69be9cf74a33c77dcbb04fe89a18850edddb
serkam31 <223928104+serkam31@users.noreply.github.com>
2025-10-31 12:53:02 +0000

    feat:  function that convert a string to an integer

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,27 @@
+#include"main.h"
+/**
+ * 
+ * 
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+	int sign = 1;
+	int result = 0;
+
+	while(s[i] != '\0' && !(s[i] >= '0' && s[i] <= '9'))
+	{
+		if (s[i] == '-')
+		{
+			sign = - sign;
+		}
+		i++;
+	}
+	
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
+	return (result * sign);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -4,4 +4,9 @@
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 
+
+
+
+
+int _atoi(char *s);
 #endif
