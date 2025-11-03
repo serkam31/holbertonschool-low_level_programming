db1491179ce163b8db0f21c76af8671d7a008f8e
serkam31 <223928104+serkam31@users.noreply.github.com>
2025-11-03 13:26:16 +0000

    fix: betty style

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -9,23 +9,23 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-    unsigned int i = 0, j;
-    int found;
+	unsigned int i = 0, j;
+	int found;
 
-    while (s[i])
-    {
-        found = 0;
-        for (j = 0; accept[j]; j++)
-        {
-            if (s[i] == accept[j])
-            {
-                found = 1;
-                break;
-            }
-        }
-        if (!found)
-            break;
-        i++;
-    }
-    return i;
+	while (s[i])
+	{
+		found = 0;
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				found = 1;
+				break;
+			}
+		}
+		if (!found)
+			break;
+		i++;
+	}
+	return (i);
 }
