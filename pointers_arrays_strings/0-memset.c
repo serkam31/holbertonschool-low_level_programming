482392e9d5771129e698e61e1ca5e8364b1aad23
serkam31 <223928104+serkam31@users.noreply.github.com>
2025-11-03 09:55:35 +0000

    feat: function that fills memory with a constant byte

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memset - remplit une zone mémoire avec une valeur constante
+ * @s: pointeur vers la zone mémoire à remplir
+ * @b: valeur constante (byte) à copier
+ * @n: nombre d’octets à écrire
+ *
+ * Return: pointeur vers la zone mémoire s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+    unsigned int i;
+
+    for (i = 0; i < n; i++)
+    {
+        s[i] = b;
+    }
+
+    return (s);
+}
