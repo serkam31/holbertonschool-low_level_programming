10bca43e0b2a82c6e066338a52411b1e5e7dcc69
serkam31 <223928104+serkam31@users.noreply.github.com>
2025-11-03 14:24:11 +0000

    fix: betty style

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,20 +1,24 @@
 #include"main.h"
 /**
- * 
- * 
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: string to search in
+ * @accept: string containing the characters to match
+ *
+ * Return: pointer to the first occurrence in s of any of the bytes in accept,
+ *         or NULL if no such byte is found
  */
 char *_strpbrk(char *s, char *accept)
 {
-    unsigned int i, j;
+	unsigned int i, j;
 
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        for (j = 0; accept[j] != '\0'; j++)
-        {
-            if (s[i] == accept[j])
-                return(&s[i]);
-        }
-        
-    }
-    return(0);   
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				return (&s[i]);
+		}
+
+	}
+	return (0);
 }
