1a7158909fd6568fe8eb497bb402d72e6877de4b
serkam31 <223928104+serkam31@users.noreply.github.com>
2025-11-04 09:24:28 +0000

    fix: betty style

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -12,24 +12,19 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 
-	// Si needle est vide, on renvoie haystack directement
 	if (*needle == '\0')
 		return (haystack);
 
-	// Parcours du haystack
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		// Si les caractères correspondent
 		for (j = 0; needle[j] != '\0'; j++)
 		{
 			if (haystack[i + j] != needle[j])
 				break;
 		}
-		// Si on a atteint la fin du needle, c'est qu'on l'a trouvé
 		if (needle[j] == '\0')
 			return (&haystack[i]);
 	}
 
-	// Aucun match trouvé
 	return (NULL);
 }
