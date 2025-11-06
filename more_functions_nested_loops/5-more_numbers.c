3742fdce479a0234dd54886ad1aa3945542447b0
serkam31 <223928104+serkam31@users.noreply.github.com>
2025-11-06 15:18:33 +0000

    feat: multiple two numbers from command-line arguments

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -12,8 +12,11 @@ void more_numbers(void)
 		for (n = 0; n <= 14; n++)
 		{
 			if (n >= 10)
+			{
 				_putchar((n / 10) + '0');
-			_putchar((n % 10) + '0');
+			}
+
+				_putchar((n % 10) + '0');
 		}
 		_putchar('\n');
 	}
