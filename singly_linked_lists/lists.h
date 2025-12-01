eba6ec0cc45d645651611ba1116dbb4fab150e74
serkam31 <223928104+serkam31@users.noreply.github.com>
2025-12-01 15:39:56 +0000

    fix: update print_list function documentation for betty doc

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -16,7 +16,10 @@ typedef struct list_s
 } list_t;
 
 /**
- * Prototype of function
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to the head of the list
+ *
+ * Return: the number of nodes in the list
  */
 size_t print_list(const list_t *h);
 
