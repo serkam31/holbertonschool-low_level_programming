9ec62395df4ad7a2fa3615a53e26dadda108aab6
serkam31 <223928104+serkam31@users.noreply.github.com>
2025-12-03 09:45:56 +0000

    feat: implement add_node and add_node_end functions for linked list management

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,58 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node_end - adds a new node at the end of a list_t list
+ * @head: double pointer to the head of the list
+ * @str: string to be duplicated and added to the new node
+ *
+ * Return: address of the new element, or NULL if it failed
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node;
+	list_t *temp;
+	unsigned int len = 0;
+
+	/* Check if str is NULL */
+	if (str == NULL)
+		return (NULL);
+
+	/* Calculate the length of str */
+	while (str[len])
+		len++;
+
+	/* Allocate memory for the new node */
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	/* Duplicate the string */
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->len = len;
+	new_node->next = NULL;
+
+	/* If the list is empty, make the new node the head */
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+
+	/* Otherwise, traverse to the end of the list */
+	temp = *head;
+	while (temp->next != NULL)
+		temp = temp->next;
+
+	/* Add the new node at the end */
+	temp->next = new_node;
+
+	return (new_node);
+}
--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -19,10 +19,11 @@ typedef struct list_s
 /**
  * print_list - prints all the elements of a list_t list
  * @h: pointer to the head of the list
- *
  * Return: the number of nodes in the list
  */
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
 
 #endif
