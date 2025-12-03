216c54a7f9d6f847847e83d24841471427b6a3f9
serkam31 <223928104+serkam31@users.noreply.github.com>
2025-12-03 09:59:55 +0000

    fix: add missing newline at end of lists.h file

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -9,3 +9,21 @@
  * @next: points to the next node
  * Description: singly linked list node structure
  */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to the head of the list
+ * Return: the number of nodes in the list
+ */
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+
+#endif
