f4ad9b489bfd606f02234afb7e3c955bfd6904e9
serkam31 <223928104+serkam31@users.noreply.github.com>
2025-12-03 17:37:23 +0000

    refactor: clean up add_node function and remove unused files

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,48 +1,35 @@
-#include <stdlib.h>
-#include <string.h>
 #include "lists.h"
+#include <string.h>
+#include <stdlib.h>
 
 /**
  * add_node - adds a new node at the beginning of a list_t list
- * @head: pointer to the pointer of the head node
- * @str: string to duplicate and store in the new node
+ * @head: pointer to the pointer of the head of the list
+ * @str: string to be duplicated
  * Return: address of the new element, or NULL if it failed
  */
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new_node;
-    char *dup;
-    unsigned int len = 0;
+	list_t *new_node;
+	unsigned int len = 0;
 
-    if (head == NULL)
-        return (NULL);
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    if (str != NULL)
-    {
-        dup = strdup(str);
-        if (dup == NULL)
-            return (NULL);
-        while (dup[len] != '\0')
-            len++;
-    }
-    else
-    {
-        dup = NULL;
-        len = 0;
-    }
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-    {
-        if (dup)
-            free(dup);
-        return (NULL);
-    }
+	while (str[len] != '\0')
+		len++;
+	new_node->len = len;
 
-    new_node->str = dup;
-    new_node->len = len;
-    new_node->next = *head;
-    *head = new_node;
+	new_node->next = *head;
+	*head = new_node;
 
-    return (new_node);
+	return (new_node);
 }
